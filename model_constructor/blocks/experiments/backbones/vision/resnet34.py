79be52fa1f8eb9113508247ecaa62a8eb95a6ad7
KyunHwan <kyunhwan.peter@gmail.com>
2026-01-19 10:09:35 +0900

    updated vfp gate and added resnet34 for data curation

---
+++ model_constructor/blocks/experiments/backbones/vision/resnet34.py
@@ -0,0 +1,32 @@
+import torchvision.models as models
+import torch.nn as nn
+import einops
+import torch.nn.functional as F
+
+class Resnet34(nn.Module):
+    def __init__(self, resize: list[int, int] | None):
+        super().__init__()
+        self.model = models.resnet34(pretrained=True)
+        self.image_resize = resize
+    
+    def forward(self, image):
+        """
+        Args:
+            image: needs to be (B, C, H, W)
+        """
+        if image.max().item() > 1.5:
+            image = image / 255.0
+
+        if len(image.shape) == 3:
+            image = einops.rearrange(image, 'c h w -> 1 c h w')
+            
+        if self.image_resize is not None:
+            h, w = self.image_resize
+            image = F.interpolate(
+                        image,
+                        size=(h, w),
+                        mode="bilinear",
+                        align_corners=False,
+                    )
+        
+        return self.model(image)
--- model_constructor/blocks/experiments/variational_flow_matching_policy/gate.py
+++ model_constructor/blocks/experiments/variational_flow_matching_policy/gate.py
@@ -1,29 +1,32 @@
 import torch
 import torch.nn as nn
 import einops
+import torch.nn.functional as F
+import math
 
 class Gate(nn.Module):
-    def __init__(self, input_dim, num_experts):
+    def __init__(self, input_dim, num_experts, temperature, top_k):
         super().__init__()
 
         self.input_dim = input_dim
         self.num_experts = num_experts
+        self.temperature = temperature
+        self.top_k = top_k
 
         self.model = nn.Sequential(
             *[
                 nn.Linear(self.input_dim, self.input_dim * 2),
                 nn.LayerNorm(self.input_dim * 2),
                 nn.ELU(),
-                nn.Linear(self.input_dim * 2, self.input_dim),
-                nn.LayerNorm(self.input_dim),
+                nn.Linear(self.input_dim * 2, self.input_dim * 2),
+                nn.LayerNorm(self.input_dim * 2),
                 nn.ELU(),
-                nn.Linear(self.input_dim, self.num_experts),
+                nn.Linear(self.input_dim * 2, self.num_experts),
                 nn.LayerNorm(self.num_experts),
             ]
         )
-        self.last_gating_func = nn.Softmax(dim=1)
         
-    def forward(self, input):
+    def forward(self, input: torch.Tensor, iterations: int, training: bool=False, use_noise: bool=False,):
         """
         Args:
             input: (batch, feature_dim) shape
@@ -31,8 +34,17 @@ class Gate(nn.Module):
         Return:
             (batch, num_experts) shape
         """
+
         if len(input.shape) == 3:
             input = einops.rearrange(input, 'b 1 d -> b d')
-        input = nn.functional.normalize(input, dim=1)
-        return self.last_gating_func(self.model(input))
 
+        clean_logits = self.model(input)
+
+        if training and use_noise:
+            noisy_logits = clean_logits + torch.randn_like(clean_logits)
+        else:
+            noisy_logits = clean_logits
+
+        router_probs = F.softmax(noisy_logits / self.temperature, dim=-1)
+
+        return router_probs
