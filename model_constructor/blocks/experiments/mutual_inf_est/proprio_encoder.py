05f9b8db03a5db4fe0f6c701e980747f291dbf1a
KyunHwan <kyunhwan.peter@gmail.com>
2026-01-19 16:15:30 +0900

    modified proprio_encoder for mutual information estimator

--- model_constructor/blocks/experiments/mutual_inf_est/proprio_encoder.py
+++ model_constructor/blocks/experiments/mutual_inf_est/proprio_encoder.py
@@ -1,2 +1,16 @@
 import torch
 import torch.nn as nn
+
+class ProprioEncoder(nn.Module):
+    def __init__(self, 
+                 input_dim: int,
+                 num_layers: int, 
+                 hidden_dim: int,
+                 output_dim: int):
+        self.input_dim = input_dim
+        self.num_layers = num_layers
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
+
+    def forward(self, input):
+        
